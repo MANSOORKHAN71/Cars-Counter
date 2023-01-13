ad6bf6ba49f9f2bec40813b9737be5239c9a2117
Mansoor Khan <mansoorkhandon71@gmail.com>
2023-01-13 15:31:14 +0500

    by ilyaskhan

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 //function increment () {
-    //console.log("the button was clicked")
+//console.log("the button was clicked")
 //}
 
-let count = 0 ;
-let countel = document.getElementById("count-el")
-let saveEL = document.getElementById("save-el")
-let totalEL = document.getElementById("total-el")
+let count = 0;
+let countel = document.getElementById('add-el');
+let saveEL = document.getElementById('save-el');
+let totalEL = document.getElementById('total-el');
 
+const increment = () => {
+  count += 1;
+  countel.innerText = count;
+};
 
-function  increment() {
-    count += 1
-    countel.innerText = count
-  
+const save = () => {
+  const previous = (saveEL.innerText || '')
+    .split('-')
+    .filter((count) => Number.isInteger(Number(count)));
+  previous.push(count);
+  saveEL.innerText = previous.join('-');
+  totalEL.innerText = Number(totalEL.innerText || 0) + count;
+  countel.textContent = 0;
+  count = 0;
+  console.log(count);
+};
+
+function reset() {
+  document.getElementById('counter').reset();
 }
-
-function save() {
-    countstr = count + " - " 
-    saveEL.textContent += countstr
-    countel.textContent = 0
-    count = 0
-    console.log(count)
-    
-}
-
-
+countel.innerText = count;
